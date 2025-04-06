4b21adc485ef7efbddaf6fae66e8400a24a1e642
nirre55 <mehdioulmi@gmail.com>
2025-04-06 11:23:31 -0400

    Add interactor

---
+++ interactor/src/interact.rs
@@ -0,0 +1,425 @@
+#![allow(non_snake_case)]
+
+pub mod config;
+mod proxy;
+
+use config::Config;
+use multiversx_sc_snippets::imports::*;
+use serde::{Deserialize, Serialize};
+use std::{
+    io::{Read, Write},
+    path::Path,
+};
+
+const STATE_FILE: &str = "state.toml";
+
+pub async fn world_forge_cli() {
+    env_logger::init();
+
+    let mut args = std::env::args();
+    let _ = args.next();
+    let cmd = args.next().expect("at least one argument required");
+    let config = Config::new();
+    let mut interact = ContractInteract::new(config).await;
+    match cmd.as_str() {
+        "deploy" => interact.deploy().await,
+        "upgrade" => interact.upgrade().await,
+        "createNft" => interact.create_nft().await,
+        "issueToken" => interact.issue_token().await,
+        "setLocalRoles" => interact.set_local_roles().await,
+        "buyNft" => interact.buy_nft().await,
+        "buyPack" => interact.buy_pack().await,
+        "setPackPrice" => interact.set_pack_price().await,
+        "getNftMetadata" => interact.get_nft_metadata().await,
+        "commonItems" => interact.common_items().await,
+        "uncommonItems" => interact.uncommon_items().await,
+        "rareItems" => interact.rare_items().await,
+        "epicItems" => interact.epic_items().await,
+        "legendaryItems" => interact.legendary_items().await,
+        "getNftName" => interact.get_nft_name().await,
+        "clearAllStorage" => interact.clean_all_sotrage().await,
+        "fillAll" => interact.fill_all().await,
+        "getNftAttributes" => interact.get_nft_attributes().await,
+        _ => panic!("unknown command: {}", &cmd),
+    }
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct State {
+    contract_address: Option<Bech32Address>
+}
+
+impl State {
+        // Deserializes state from file
+        pub fn load_state() -> Self {
+            if Path::new(STATE_FILE).exists() {
+                let mut file = std::fs::File::open(STATE_FILE).unwrap();
+                let mut content = String::new();
+                file.read_to_string(&mut content).unwrap();
+                toml::from_str(&content).unwrap()
+            } else {
+                Self::default()
+            }
+        }
+    
+        /// Sets the contract address
+        pub fn set_address(&mut self, address: Bech32Address) {
+            self.contract_address = Some(address);
+        }
+    
+        /// Returns the contract address
+        pub fn current_address(&self) -> &Bech32Address {
+            self.contract_address
+                .as_ref()
+                .expect("no known contract, deploy first")
+        }
+    }
+    
+    impl Drop for State {
+        // Serializes state to file
+        fn drop(&mut self) {
+            let mut file = std::fs::File::create(STATE_FILE).unwrap();
+            file.write_all(toml::to_string(self).unwrap().as_bytes())
+                .unwrap();
+        }
+    }
+
+pub struct ContractInteract {
+    interactor: Interactor,
+    wallet_address: Address,
+    contract_code: BytesValue,
+    state: State
+}
+
+impl ContractInteract {
+    pub async fn new(config: Config) -> Self {
+        let mut interactor = Interactor::new(config.gateway_uri())
+            .await
+            .use_chain_simulator(config.use_chain_simulator());
+
+        interactor.set_current_dir_from_workspace("world-forge");
+        let wallet_address = interactor.register_wallet(test_wallets::alice()).await;
+
+        // Useful in the chain simulator setting
+        // generate blocks until ESDTSystemSCAddress is enabled
+        interactor.generate_blocks_until_epoch(1).await.unwrap();
+        
+        let contract_code = BytesValue::interpret_from(
+            "mxsc:../output/world-forge.mxsc.json",
+            &InterpreterContext::default(),
+        );
+
+        ContractInteract {
+            interactor,
+            wallet_address,
+            contract_code,
+            state: State::load_state()
+        }
+    }
+
+    pub async fn deploy(&mut self) {
+        let new_address = self
+            .interactor
+            .tx()
+            .from(&self.wallet_address)
+            .gas(300_000_000u64)
+            .typed(proxy::NftMinterProxy)
+            .init()
+            .code(&self.contract_code)
+            .returns(ReturnsNewAddress)
+            .run()
+            .await;
+        let new_address_bech32 = bech32::encode(&new_address);
+        self.state
+            .set_address(Bech32Address::from_bech32_string(new_address_bech32.clone()));
+
+        println!("new address: {new_address_bech32}");
+    }
+
+    pub async fn upgrade(&mut self) {
+        let response = self
+            .interactor
+            .tx()
+            .to(self.state.current_address())
+            .from(&self.wallet_address)
+            .gas(30_000_000u64)
+            .typed(proxy::NftMinterProxy)
+            .upgrade()
+            .code(&self.contract_code)
+            .code_metadata(CodeMetadata::UPGRADEABLE)
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {response:?}");
+    }
+
+    pub async fn create_nft(&mut self) {
+        let selling_price = BigUint::<StaticApi>::from(0u128);
+        let opt_token_used_as_payment = OptionalValue::Some(TokenIdentifier::from_esdt_bytes(&b""[..]));
+        let opt_token_used_as_payment_nonce = OptionalValue::Some(0u64);
+
+        let response = self
+            .interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(self.state.current_address())
+            .gas(30_000_000u64)
+            .typed(proxy::NftMinterProxy)
+            .create_nft(selling_price, opt_token_used_as_payment, opt_token_used_as_payment_nonce)
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {response:?}");
+    }
+
+    pub async fn issue_token(&mut self) {
+        let egld_amount = BigUint::<StaticApi>::from(0u128);
+
+        let token_name = ManagedBuffer::new_from_bytes(&b""[..]);
+        let token_ticker = ManagedBuffer::new_from_bytes(&b""[..]);
+
+        let response = self
+            .interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(self.state.current_address())
+            .gas(30_000_000u64)
+            .typed(proxy::NftMinterProxy)
+            .issue_token(token_name, token_ticker)
+            .egld(egld_amount)
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {response:?}");
+    }
+
+    pub async fn set_local_roles(&mut self) {
+        let response = self
+            .interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(self.state.current_address())
+            .gas(30_000_000u64)
+            .typed(proxy::NftMinterProxy)
+            .set_local_roles()
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {response:?}");
+    }
+
+    pub async fn buy_nft(&mut self) {
+        let token_id = String::new();
+        let token_nonce = 0u64;
+        let token_amount = BigUint::<StaticApi>::from(0u128);
+
+        let nft_nonce = 0u64;
+
+        let response = self
+            .interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(self.state.current_address())
+            .gas(30_000_000u64)
+            .typed(proxy::NftMinterProxy)
+            .buy_nft(nft_nonce)
+            .payment((TokenIdentifier::from(token_id.as_str()), token_nonce, token_amount))
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {response:?}");
+    }
+
+    pub async fn buy_pack(&mut self) {
+        let egld_amount = BigUint::<StaticApi>::from(0u128);
+
+        let response = self
+            .interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(self.state.current_address())
+            .gas(30_000_000u64)
+            .typed(proxy::NftMinterProxy)
+            .buy_pack()
+            .egld(egld_amount)
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {response:?}");
+    }
+
+    pub async fn set_pack_price(&mut self) {
+        let price = BigUint::<StaticApi>::from(0u128);
+
+        let response = self
+            .interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(self.state.current_address())
+            .gas(30_000_000u64)
+            .typed(proxy::NftMinterProxy)
+            .set_pack_price(price)
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {response:?}");
+    }
+
+    pub async fn get_nft_metadata(&mut self) {
+        let nonce = 0u64;
+
+        let result_value = self
+            .interactor
+            .query()
+            .to(self.state.current_address())
+            .typed(proxy::NftMinterProxy)
+            .get_nft_metadata(nonce)
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {result_value:?}");
+    }
+
+    pub async fn common_items(&mut self) {
+        let result_value = self
+            .interactor
+            .query()
+            .to(self.state.current_address())
+            .typed(proxy::NftMinterProxy)
+            .common_items()
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {result_value:?}");
+    }
+
+    pub async fn uncommon_items(&mut self) {
+        let result_value = self
+            .interactor
+            .query()
+            .to(self.state.current_address())
+            .typed(proxy::NftMinterProxy)
+            .uncommon_items()
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {result_value:?}");
+    }
+
+    pub async fn rare_items(&mut self) {
+        let result_value = self
+            .interactor
+            .query()
+            .to(self.state.current_address())
+            .typed(proxy::NftMinterProxy)
+            .rare_items()
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {result_value:?}");
+    }
+
+    pub async fn epic_items(&mut self) {
+        let result_value = self
+            .interactor
+            .query()
+            .to(self.state.current_address())
+            .typed(proxy::NftMinterProxy)
+            .epic_items()
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {result_value:?}");
+    }
+
+    pub async fn legendary_items(&mut self) {
+        let result_value = self
+            .interactor
+            .query()
+            .to(self.state.current_address())
+            .typed(proxy::NftMinterProxy)
+            .legendary_items()
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {result_value:?}");
+    }
+
+    pub async fn get_nft_name(&mut self) {
+        let storage_index = 0u32;
+
+        let result_value = self
+            .interactor
+            .query()
+            .to(self.state.current_address())
+            .typed(proxy::NftMinterProxy)
+            .get_nft_name(storage_index)
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {result_value:?}");
+    }
+
+    pub async fn clean_all_sotrage(&mut self) {
+        let response = self
+            .interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(self.state.current_address())
+            .gas(30_000_000u64)
+            .typed(proxy::NftMinterProxy)
+            .clean_all_sotrage()
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {response:?}");
+    }
+
+    pub async fn fill_all(&mut self) {
+        let response = self
+            .interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(self.state.current_address())
+            .gas(30_000_000u64)
+            .typed(proxy::NftMinterProxy)
+            .fill_all()
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {response:?}");
+    }
+
+    pub async fn get_nft_attributes(&mut self) {
+        let nft_nonce = 0u64;
+
+        let result_value = self
+            .interactor
+            .query()
+            .to(self.state.current_address())
+            .typed(proxy::NftMinterProxy)
+            .get_nft_attributes(nft_nonce)
+            .returns(ReturnsResultUnmanaged)
+            .run()
+            .await;
+
+        println!("Result: {result_value:?}");
+    }
+
+}
